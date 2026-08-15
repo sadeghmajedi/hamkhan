6c8e5c1fbc986367996071775c80fa52f2cdfb00
sadeghmajedi <sadeghmajedi@gmail.com>
2026-08-15 15:02:20 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hamkhan-cache-v0.4.0';
+const CACHE_NAME = 'hamkhan-cache-v0.5.0';
 const APP_SHELL = [
   './',
   './index.html',
@@ -33,5 +33,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
-
